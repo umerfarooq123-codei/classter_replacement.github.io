f63ad0d0f0dbaba44bc6c8d1ccd057135f4bebfa
Muhammad Umer Farooq <74726720+umerfarooq123-codei@users.noreply.github.com>
2024-11-24 00:31:55 +0500

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3dbafa860cbbfc840eb3fb396ddc342d
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8833c6a8a7209240a4c94e6f370ae975",
+"flutter_bootstrap.js": "78f98f5ba43ba8f2cadbecf346f98775",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
