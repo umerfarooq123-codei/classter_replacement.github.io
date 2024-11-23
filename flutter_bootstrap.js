2118430e22073042cfc4f5eb13bc92c829728115
Muhammad Umer Farooq <74726720+umerfarooq123-codei@users.noreply.github.com>
2024-11-24 00:25:31 +0500

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "857493359"
+    serviceWorkerVersion: "3647136095"
   }
 });
