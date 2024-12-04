db02d24d7ea1b82050e0c0d0c81bcada9e44ce3d
Muhammad Umer Farooq <74726720+umerfarooq123-codei@users.noreply.github.com>
2024-12-05 04:45:03 +0500

    home_page and other pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1011287132"
+    serviceWorkerVersion: "2330091217"
   }
 });
