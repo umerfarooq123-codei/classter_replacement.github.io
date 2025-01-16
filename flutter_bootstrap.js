5ce4fa3df812e58af6dc5ffae965cd4b199f2f2b
Muhammad Umer Farooq <74726720+umerfarooq123-codei@users.noreply.github.com>
2025-01-16 22:31:12 +0500

    new designs and pages added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2203672174"
+    serviceWorkerVersion: "3085407434"
   }
 });
